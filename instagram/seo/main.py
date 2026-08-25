de4faf0de617c1cee0bb87fc9fb440d9fd9e92a1
Irk Lost <284513799+irklost@users.noreply.github.com>
2026-08-24 22:31:15 -0400

    feat: proof-of-concept completed

--- instagram/seo/main.py
+++ instagram/seo/main.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
 
 import json
+import random
 from rich import print
 
 data_json = "data.json"
 
+min_ht = 13
+max_ht = 19
+
+min_kw = 11
+max_kw = 17
+
 def main():
     with open(data_json, "r") as file:
         data = json.load(file)
-    #print(json.dumps(data,indent=4))
-    print(len(data))
-    print(data.keys())
+    cats = list(data.keys())
+    for idx,cat in enumerate(cats):
+        print(idx, cat)
+    cat_choice = int(input())
+    if cat_choice < 0 or cat_choice >= len(cats):
+        cat_choice = 0
+    ht = list(data[cats[cat_choice]].keys())[0]
+    kw = list(data[cats[cat_choice]].keys())[1]
+    ht_all = list(data[cats[cat_choice]][ht])
+    kw_all = list(data[cats[cat_choice]][kw])
+    ht_num = random.randint(min_ht,max_ht)
+    kw_num = random.randint(min_kw,max_kw)
+    ht_list = random.choices(ht_all,k=ht_num)
+    kw_list = random.choices(kw_all,k=kw_num)
+    print(f"\n\n---COPY AND PASTE THE BELOW LINES FOR A <<< {cats[cat_choice]} >>> POST. CAPTION FOR POST, COMMENTS FOR STORIES AND REELS---\n\n")
+    print(" ".join(ht_list))
+    print(", ".join(kw_list))
 
 
 if __name__ == "__main__":
